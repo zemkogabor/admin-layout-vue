1955994e808e2ff7a9fc3e080dc179d4f276e3fe
Gabor Zemko <gaborzemko@gmail.com>
2024-03-01 18:49:27 +0100

    brand devs revert

--- src/AdminLayout.vue
+++ src/AdminLayout.vue
@@ -43,13 +43,13 @@
       </div>
     </nav>
     <nav class="navbar navbar-left" :class="[leftNavClass, leftNavBgClass]" :data-bs-theme="dataBsThemeLeft">
-      <slot name="brand">
-        <div v-if="brandName" class="navbar-brand">
-          <span class="brand-text">
+      <div class="navbar-brand">
+        <slot name="brand">
+          <span v-if="brandName" class="brand-text">
             {{ brandName }}
           </span>
-        </div>
-      </slot>
+        </slot>
+      </div>
       <div class="navbar-nav">
         <div v-for="(group, groupIndex) in leftItemsByGroups" :key="groupIndex" class="group-wrapper">
           <div v-if="group.heading" class="group-heading-text text-uppercase mb-2">
